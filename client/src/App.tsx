cc13eb42e2efa5ab814826a8fdd3d1b10025196e
TharakaJayz <tharakaprabhath300@gmail.com>
2024-02-18 16:50:15 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-red-100 w-screen h-screen test_style">
+     facebook
+    </div>
+  );
+}
+
+export default App;
